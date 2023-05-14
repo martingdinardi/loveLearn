c60152b0cd8c359d1e633989014b862c11490a43
martingdinardi <martingdinardi@gmail.com>
2023-05-13 23:36:38 -0300

    LL-03: Courses created and fully styled.

---
+++ src/components/Main/LenguageCard/LenguageCard.tsx
@@ -0,0 +1,105 @@
+import { styled } from "styled-components";
+import CourseCard from "../CourseCard/CourseCard";
+
+interface Course {
+  id?: string;
+  img?: string;
+  title?: string;
+  description?: string;
+  link?: string;
+  author?: string;
+  author_logo?: string;
+  author_web?: string;
+  video_link?: string;
+}
+
+interface LenguageCard {
+  img?: string;
+  number?: string;
+  title?: string;
+  description?: string;
+  color?: string;
+  courses?: Course[];
+}
+
+const LenguageCardContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  padding: 0 0 5rem 0;
+  .lenguage-card-image {
+    display: flex;
+    align-items: start;
+  }
+  .lenguage-card-description {
+    small {
+      font-size: 1.7rem;
+      color: lightgray;
+    }
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+`;
+
+const LenguageCardContent = styled.div`
+  margin: 2rem 0;
+  li {
+    list-style: none;
+    margin: 1rem 0;
+  }
+  h4 {
+    width: 25%;
+    padding: 1rem 0 0.2rem 0;
+    border-radius: 2px;
+    border-bottom: 0.5rem solid;
+    margin-bottom: 1.5rem;
+  }
+`;
+
+const LenguageCardCourses = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  flex-wrap: wrap;
+`;
+
+function LenguageCard(props: LenguageCard) {
+  return (
+    <LenguageCardContainer>
+      <div className="lenguage-card-image">
+        <img src={props.img} width={"100px"} />
+      </div>
+      <div className="lenguage-card-header">
+        <div className="lenguage-card-description">
+          <div>
+            <small>{props.number}</small>
+            <h3>{props.title}</h3>
+          </div>
+          <p>{props.description}</p>
+        </div>
+        <LenguageCardContent>
+          <h4 style={{ borderColor: props.color }}>Cursos</h4>
+          <LenguageCardCourses>
+            {props.courses &&
+              props.courses.map(({ img, title, description, author_logo, author, author_web, link }) => {
+                return (
+                  <>
+                    <CourseCard
+                      img={img}
+                      title={title}
+                      description={description}
+                      author_logo={author_logo}
+                      author={author}
+                      author_web={author_web}
+                      link={link}
+                    />
+                  </>
+                );
+              })}
+          </LenguageCardCourses>
+        </LenguageCardContent>
+      </div>
+    </LenguageCardContainer>
+  );
+}
+
+export default LenguageCard;
